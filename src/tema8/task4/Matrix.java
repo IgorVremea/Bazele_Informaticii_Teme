b484c588d948e8b55be6daf46d553966e143cf6d
Igor <igori.vremya@gmail.com>
2026-04-04 01:55:55 +0300

    Tema 8 task 4

--- src/tema8/task4/Matrix.java
+++ src/tema8/task4/Matrix.java
@@ -1,36 +1,45 @@
 // package tema8.task4;
+
 import java.io.*;
 class Matrix{
   public static void main(String[] args){
     try{
-      File fileInput = new File("test.txt");
-      BufferedReader consoleInput = new BufferedReader(new InputStreamReader(new FileInputStream(fileInput), "UTF-8"));
+      BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
+      System.out.print("Enter n m p (separated by space): ");
       String line = consoleInput.readLine();
       String[] sizesArr = line.split(" ");
       int n = Integer.parseInt(sizesArr[0]);
       int m = Integer.parseInt(sizesArr[1]);
       int p = Integer.parseInt(sizesArr[2]);
-      System.out.println(n + " " + m + " " + p);
       
       double[][] mx1 = new double[n][m];
       double[][] mx2 = new double[m][p];
       double[][] mxRes = new double[n][p];
       
+      System.out.println("!Enter every matrix line by line!");
+      System.out.println("Matrix #1:");
       for(int ln = 0; ln < n; ln++){
+        System.out.print("\t");
         line = consoleInput.readLine();
         String[] nums = line.split(" ");
         for(int col = 0; col < m; col++){
           mx1[ln][col] = Double.parseDouble(nums[col]);
         }
       }
+      
+      System.out.println("\nMatrix #2:");
       for(int ln = 0; ln < m; ln++){
+        System.out.print("\t");
         line = consoleInput.readLine();
         String[] nums = line.split(" ");
         for(int col = 0; col < p; col ++){
           mx2[ln][col] = Double.parseDouble(nums[col]);
         }
       }
+      
+      System.out.println("\nResult Matrx #1 X Matrix #2:");
       for(int ln = 0; ln < n; ln++){
+        System.out.print("\t");
         for(int col = 0; col < p; col++){
           for(int i = 0; i < m; i++){
             mxRes[ln][col] += mx1[ln][i] * mx2[i][col];
@@ -39,8 +48,6 @@ class Matrix{
         }
         System.out.println();
       }
-      
-      consoleInput.close();  
     } catch (Exception e){
       e.printStackTrace();
     }
